fc14dd0e918d445de60c1ae27180a75212cd6998
pooja sunkara <sunkara.pooja226@gmail.com>
2026-06-10 20:02:07 +0530

    updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import AboutPlatform from "./components/AboutPlatform";
-import HowItWorks from "./components/HowItWorks";
-import Trends from "./components/Trends";
-import Future from "./components/Future";
 import Projects from "./pages/Projects";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
-        <Route
-          path="/AICortex-Studio/"
-          element={<Navigate to="/AICortex-Studio/home" replace />}
-        />
-        <Route path="/AICortex-Studio/home" element={<Home />} />
-        <Route path="/AICortex-Studio/about" element={<About />} />
-        <Route path="/AICortex-Studio/projects" element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
